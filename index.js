eacce7cc6bc377d8f39b25a5f6a8d207a360bde5
SP20-BCS-A-007 <sp20-bcs-007@cuilahore.edu.pk>
2022-11-22 16:44:11 +0500

    commit 1 submission

---
+++ index.js
@@ -0,0 +1,14 @@
+// window.onload= ()=>{
+//     document.getElementById("btn").addEventListener('click', ()=>{
+//             alert("hello")
+//     }
+//     )
+// }
+
+$(function(){
+$("#btn").click(hello);
+})
+
+let hello= ()=>{
+    alert("Asslamualikum Sir")
+}
